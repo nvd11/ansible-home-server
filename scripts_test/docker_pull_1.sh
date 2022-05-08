22c00be9f09fb194e29077f9bfa39cc4c1e3ccb4
nvd11 <nvd11@163.com>
2022-05-08 23:58:25 +0800

    ANS-7 #comment complete

--- scripts_test/docker_pull_1.sh
+++ scripts_test/docker_pull_1.sh
@@ -5,6 +5,7 @@ ansible-playbook -e "container_name=test_hsbc_app6" -vv docker_stop.yml
 ansible-playbook -e "container_name=test_hsbc_app6" -vv docker_rm.yml
 
 
-nsible-playbook -e "docker_host=registry.cn-hangzhou.aliyuncs.com/hsbc_jason/ repo=spring tag=0.4" -vv docker_pull.yml
+ansible-playbook -e "docker_host=registry.cn-hangzhou.aliyuncs.com/hsbc_jason/ repo=spring tag=0.4" -vv docker_pull.yml
+ansible-playbook -e "container_name=test_hsbc_app6 port_map=3351:8081 docker_host=registry.cn-hangzhou.aliyuncs.com/hsbc_jason/ repo=spring tag=0.4" -vv docker_run.yml
 
-docker run -d -p 3351:8081 --name test_hsbc_app6 8adf4ad67700
+docker run -d -p 3351:8081 --name test_hsbc_app6 registry.cn-hangzhou.aliyuncs.com/hsbc_jason/spring:0.4
