afd33edabf54a64fb618477d1a530b59093a4e32
nvd11 <nvd11@163.com>
2022-07-11 02:15:41 +0800

    INS-1 #comment test docker install mysql 8

--- playbooks/mysql/Create_dabase.sql
+++ playbooks/mysql/Create_dabase.sql
@@ -1,6 +1,6 @@
 Create Database If Not Exists demo_spring  default character set utf8 collate utf8_general_ci;
 
-create user If Not Exists nvd11@'%' identified by '32565624';
+create user If Not Exists nvd11@'%' identified by 'hsbc1234';
 
 grant select,insert,update,delete,create on demo_spring.* to nvd11;
 
